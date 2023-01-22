f18a350268107c022c3ec2f628be42e05c28b1a3
Leon <matthe50@purdue.edu>
2023-01-22 05:01:05 -0500

    added to go through global and reset data before

--- Scripts/Start Menu.gd
+++ Scripts/Start Menu.gd
@@ -4,13 +4,18 @@ export var joinGameMenuScene : PackedScene
 export var createGameMenuScene : PackedScene
 export var mainScreenScene : PackedScene
 
+onready var globals = get_node("/root/Globals")
+
 func _on_Join_Game_Button_button_up():
-	get_tree().change_scene(joinGameMenuScene.resource_path)
+	globals.reset_data()
+	globals.goto_scene(joinGameMenuScene.resource_path)
 
 
 func _on_Create_Game_Button_button_up():
-	get_tree().change_scene(createGameMenuScene.resource_path)
+	globals.reset_data()
+	globals.goto_scene(createGameMenuScene.resource_path)
 
 
 func _on_Start_Game_Button_button_up():
-	get_tree().change_scene(mainScreenScene.resource_path)
+	globals.reset_data()
+	globals.goto_scene(mainScreenScene.resource_path)
